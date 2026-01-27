89e7ad9c56764980f79e0f0d365feaeecaca8c92
xfac11 <xfac115@gmail.com>
2026-01-27 20:25:27 +0100

    Automate the generation of the public folder taking everything from static and copying it into the newly created public folder. public folder is ignored by git

--- src/main.py
+++ src/main.py
@@ -1,7 +1,28 @@
 from textnode import*
-def main():
-    text_node = TextNode("This is important text", TextType.BOLD_TEXT_TYPE)
-    print(text_node)
-    print("hello world")
+from shutil import*
+from os import listdir
+from os import mkdir
+from os import path
 
+def copy_tree_r(source, destination):
+    if not path.exists(source):
+        raise TypeError(f"Source path: {source} does not exist")
+    if path.isfile(source):
+        return
+    dir = listdir(source)
+    for file in dir:
+        if not path.isfile(path.join(source,file)):
+            mkdir(path.join(destination,file))
+            copy_tree_r(path.join(source,file), path.join(destination, file))
+        if path.isfile(path.join(source, file)):
+            copy(path.join(source,file), destination)
+    
+
+def copy_tree(source, destination):
+    rmtree(destination)
+    mkdir(destination)
+    copy_tree_r(source, destination)
+
+def main():
+    copy_tree("static", "public")
 main()
